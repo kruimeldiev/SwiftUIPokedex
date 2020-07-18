217772521aa650857930308b2a1f7ae87eb7b7d8
Casper Daris <casperdaris@msn.com>
2020-07-18 20:05:44 +0200

    Leeg project, startpunt voor Git

---
+++ SwiftUI Pokedex/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Pokedex
+//
+//  Created by Casper Daris on 18/07/2020.
+//  Copyright © 2020 Casper Daris. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
